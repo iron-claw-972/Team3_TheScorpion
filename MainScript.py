35173a8a5eaeb4542f2786dbe6b32a17cc1cab3a
Taren P <Tarenpatel1013@gmail.com>
2020-12-14 17:21:48 -0800

    structure

--- MainScript.py
+++ MainScript.py
@@ -3,3 +3,8 @@ import RPi.GPIO as GPIO
 from mpu6050 import mpu6050
 
 mpu = mpu6050(0x68)
+
+while (gyro_data['x'] == 0){ 
+    ##we can change these numbers later. It is more of a structure.
+    #move forward
+}
