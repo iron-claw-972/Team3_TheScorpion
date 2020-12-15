2d8b658a0b02e5905ffb00646fd1751d7e170bfb
Taren P <Tarenpatel1013@gmail.com>
2020-12-14 17:26:26 -0800

    a

--- MainScript.py
+++ MainScript.py
@@ -4,7 +4,29 @@ from mpu6050 import mpu6050
 
 mpu = mpu6050(0x68)
 
-while (gyro_data['x'] == 0){ 
+ena=22
+enb=32
+
+#GPIO Pins
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
+GPIO.setup(ena,GPIO.OUT)
+
+GPIO.output(18,False)
+GPIO.output(16,False)
+GPIO.output(26,False)
+GPIO.output(24,False)
+
+p=GPIO.PWM(ena, 1000)
+p2=GPIO.PWM(enb, 1000)
+p.start(22)
+p2.start(32)
+
+if (gyro_data['x'] == 0){ 
     ##we can change these numbers later. It is more of a structure.
     #move forward
+
 }
