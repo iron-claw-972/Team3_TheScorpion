b2f141ea4c46c1886448d2d9a40c7b8e703936d7
Taren P <Tarenpatel1013@gmail.com>
2020-12-14 17:30:58 -0800

    forwards

--- MainScript.py
+++ MainScript.py
@@ -7,6 +7,8 @@ mpu = mpu6050(0x68)
 ena=22
 enb=32
 
+
+GPIO.setmode(GPIO.BOARD)
 #GPIO Pins
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT)
@@ -25,8 +27,15 @@ p2=GPIO.PWM(enb, 1000)
 p.start(22)
 p2.start(32)
 
+GPIO.setwarnings(False)
+
 if (gyro_data['x'] == 0){ 
     ##we can change these numbers later. It is more of a structure.
     #move forward
-
+    p.ChangeDutyCycle(80)#speed
+    p2.ChangeDutyCycle(80)#speed
+    GPIO.output(18,False)
+    GPIO.output(16,True)
+    GPIO.output(26,False)
+    GPIO.output(24,True)
 }
