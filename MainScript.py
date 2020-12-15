65c0d9b518c2562b5f818010852ea2100287aa7e
Taren P <Tarenpatel1013@gmail.com>
2020-12-14 18:04:03 -0800

    Final for now

--- MainScript.py
+++ MainScript.py
@@ -8,34 +8,70 @@ ena=22
 enb=32
 
 
+
 GPIO.setmode(GPIO.BOARD)
 #GPIO Pins
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
+
+pin = 
+pin2 =
+pin3 =
+pin4 =
+
+GPIO.setup(pin,GPIO.OUT)
+GPIO.setup(pin2,GPIO.OUT)
+GPIO.setup(pin3, GPIO.OUT)
+GPIO.setup(pin4,GPIO.OUT)
 GPIO.setup(enb, GPIO.OUT)
 GPIO.setup(ena,GPIO.OUT)
 
-GPIO.output(18,False)
-GPIO.output(16,False)
-GPIO.output(26,False)
-GPIO.output(24,False)
+GPIO.output(pin,False)
+GPIO.output(pin2,False)
+GPIO.output(pin3,False)
+GPIO.output(pin4,False)
 
 p=GPIO.PWM(ena, 1000)
 p2=GPIO.PWM(enb, 1000)
-p.start(22)
-p2.start(32)
+p.start(ena)
+p2.start(enb)
 
 GPIO.setwarnings(False)
 
-if (gyro_data['x'] == 0){ 
+firststage = True
+secondstage = False
+thirdstage = False
+counter = 0
+
+gyro = mpu.get_gyro_data()
+while (firststage == True):
     ##we can change these numbers later. It is more of a structure.
     #move forward
     p.ChangeDutyCycle(80)#speed
     p2.ChangeDutyCycle(80)#speed
-    GPIO.output(18,False)
-    GPIO.output(16,True)
-    GPIO.output(26,False)
-    GPIO.output(24,True)
-}
+    GPIO.output(pin,False)
+    GPIO.output(pin2,True)
+    GPIO.output(pin3,False)
+    GPIO.output(pin4,True)
+    if (gyro['x'] >15 and gyro['x'] < 120):
+        firststage = False
+        secondstage = True
+
+while (secondstage == True):
+    p.ChangeDutyCycle(80)#speed
+    p2.ChangeDutyCycle(80)#speed
+    GPIO.output(pin,False)
+    GPIO.output(pin2,True)
+    GPIO.output(pin3,False)
+    GPIO.output(pin4,True)
+    if (gyro['x'] >0 and gyro['x'] < 15):
+        secondstage = False
+        thirdstage = True
+
+if (thirdstage == True):
+    while (counter <= 20):
+        p.ChangeDutyCycle(80)#speed
+        p2.ChangeDutyCycle(80)#speed
+        GPIO.output(pin,False)
+        GPIO.output(pin2,True)
+        GPIO.output(pin3,False)
+        GPIO.output(pin4,True)
+
